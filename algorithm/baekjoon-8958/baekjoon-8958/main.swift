58dab8828b405db0f59b9b2e711ff34c637463d4
VincentGeranium <kwangjun3952@gmail.com>
2022-03-25 10:22:00 +0900

    feat: added algorithm dir.

---
+++ algorithm/baekjoon-8958/baekjoon-8958/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  baekjoon-8958
+//
+//  Created by Morgan Kang on 2022/03/24.
+//
+
+import Foundation
+
+let testCase = readLine().flatMap {Int($0)!}!
+
+
+for _ in 0..<testCase {
+    let input = readLine()!.split(separator: "X")
+    var b = 0
+//    print("input : \(input)")
+    for i in 0..<input.count {
+        let c = input[i].map {($0)}
+//        print("c : \(c)")
+        var d = c.count
+//        print("c.count : \(d)")
+        
+    }
+//    print(b)
+}
+
+ 
+
