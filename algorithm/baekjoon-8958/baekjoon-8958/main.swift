4fcede82c169d24e94cd6e4e39bcb1c0639fe709
VincentGeranium <kwangjun3952@gmail.com>
2022-03-25 10:59:52 +0900

    fix: fixed 8958 algorithm code.

--- algorithm/baekjoon-8958/baekjoon-8958/main.swift
+++ algorithm/baekjoon-8958/baekjoon-8958/main.swift
@@ -7,22 +7,29 @@
 
 import Foundation
 
-let testCase = readLine().flatMap {Int($0)!}!
+let caseNum = readLine().flatMap { Int($0) }!
 
+var results = [String]()
+var score = 0
+var stackScore = 0
 
-for _ in 0..<testCase {
-    let input = readLine()!.split(separator: "X")
-    var b = 0
-//    print("input : \(input)")
-    for i in 0..<input.count {
-        let c = input[i].map {($0)}
-//        print("c : \(c)")
-        var d = c.count
-//        print("c.count : \(d)")
-        
-    }
-//    print(b)
+for _ in 0..<caseNum {
+    let result = readLine().flatMap { $0 }!
+    results.append(result)
 }
 
+for result in results {
+    score = 0
+    stackScore = 0
+    for answer in result {
+        if answer == "O" {
+            stackScore += 1
+        } else {
+            stackScore = 0
+        }
+        score += stackScore
+    }
+    print(score)
+}
  
 
